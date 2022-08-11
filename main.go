072d27e274116ed2b6b259d97690632d41ea7b0f
Ttibsi <ashisbitt@icloud.com>
2022-08-11 09:56:05 +0100

    Count files

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -31,21 +32,47 @@ func path_trawl(dir string) []string {
 	return all_paths
 }
 
-func file_length(file string) int {
+func file_length(filepath string) int {
+	var length int = 0
 
+	if len(filepath) == 0 {
+		return 0
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		//TODO: handle commented out lines
+		if scanner.Text() != "" { // Ignore empty lines
+			length++
+		}
+	}
+
+	return length
 }
 
 func main() {
 	PLACEHOLDER := "/users/ashisbitt/workspace/gh-stats"
 	dir := PLACEHOLDER
 	var count int = 0
+	var file_count int = 0
 
 	all_files := path_trawl(dir)
 
 	for _, file := range all_files {
 		count += file_length(file)
+		if count != 0 {
+			file_count++
+		}
 	}
 
 	fmt.Println(count)
+	fmt.Println(file_count)
+	// fmt.Println("Found %s lines of code across %s files", count, file_count)
 
 }
