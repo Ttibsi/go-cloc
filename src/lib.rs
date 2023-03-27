b34e41f9beaaeaed2b7f0c87c47d25673ed138b6
Ttibsi <ashisbitt@icloud.com>
2023-03-27 12:20:06 +0100

    tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,16 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt::Write;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::{fs, path::Path};
 
-fn get_lines(inp_file: String) -> i32 {
-    let file = File::open(inp_file).expect("Unable to open file");
-    let reader = BufReader::new(file);
-    let mut line_count = 0;
-
-    for _ in reader.lines() {
-        line_count += 1;
-    }
-
-    return line_count;
-}
-
-fn check_for_files(dir: String) -> HashMap<String, i32> {
+fn check_for_files(dir: &str) -> Result<HashMap<String, i32>, Box<dyn Error>> {
     let mut results: HashMap<String, i32> = HashMap::new();
 
-    let dir_entries = match fs::read_dir(dir) {
-        Ok(entries) => entries,
-        Err(e) => {
-            println!("Error reading directory: {:?}", e);
-            return results;
-        }
-    };
-
-    for entry_result in dir_entries {
-        let entry = match entry_result {
-            Ok(e) => e,
-            Err(e) => {
-                println!("Error reading directory entry: {:?}", e);
-                continue;
-            }
-        };
-        let entry_path = entry.path();
+    for entry_result in fs::read_dir(dir)? {
+        let entry_path = entry_result?.path();
+        //
         //TODO: Ignore what's in .gitignore
         if entry_path.starts_with("./.git") {
             continue;
@@ -45,7 +21,7 @@ fn check_for_files(dir: String) -> HashMap<String, i32> {
 
         if entry_path.is_dir() {
             // Handle directory
-            results.extend(check_for_files(entry_path.to_str().unwrap().to_string()));
+            results.extend(check_for_files(entry_path.to_str().unwrap()));
         } else if entry_path.is_file() {
             // Handle file
             let ext = Path::new(entry_path.to_str().unwrap())
@@ -54,16 +30,18 @@ fn check_for_files(dir: String) -> HashMap<String, i32> {
                 .unwrap_or("")
                 .to_string();
 
-            if results.get(&ext) != None {
+            if results.get(&ext).is_some() {
                 results.insert(
                     ext.clone(),
-                    get_lines(entry_path.to_str().unwrap().to_string())
-                        + results.get(&ext.clone()).unwrap().to_owned(),
+                    (BufReader::new(File::open(entry_path)?).lines().count()
+                        + results.get(&ext.clone()).unwrap().to_owned())
+                    .try_into()
+                    .unwrap(),
                 );
             } else {
                 results.insert(
                     ext.clone(),
-                    get_lines(entry_path.to_str().unwrap().to_string()),
+                    BufReader::new(File::open(entry_path)).lines().count(),
                 );
             }
         } else {
@@ -72,14 +50,14 @@ fn check_for_files(dir: String) -> HashMap<String, i32> {
         }
     }
 
-    return results;
+    return Ok(results);
 }
 
 pub fn scan(args: Vec<String>) -> HashMap<String, i32> {
     let mut results: HashMap<String, i32> = HashMap::new();
 
     for arg in args {
-        let ret = check_for_files(arg);
+        let ret = check_for_files(&arg);
         results.extend(ret);
     }
 
@@ -95,12 +73,7 @@ pub fn print(res: HashMap<String, i32>) {
             len = k.len();
         }
 
-        output += "| ";
-        output += k;
-        output += " | ";
-        output += &v.to_string();
-        output += " |";
-        output += "\n"
+        write!(&mut output, "| {} | {} |\n", k, v.to_string()).unwrap();
     }
 
     println!("+{}+", "-".repeat(len + 7));
